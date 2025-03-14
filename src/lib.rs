09f5739ed85dd98d9d38f2a2d492a360a9272d23
Eleocraft <eliseo.cailloux@gmx.de>
2025-03-14 15:01:53 +0100

    Public export ConfigEnum

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub use config_macro::Config;
+pub use config_macro::ConfigEnum;
 
 pub trait Config {
     fn add_source<'a>(&mut self, values: impl Iterator<Item = &'a str>) -> Result<(), String>;
