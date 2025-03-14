d0cfd991d583bca7350023551fdbaec8866097ae
Eleocraft <eliseo.cailloux@gmx.de>
2025-03-14 15:30:56 +0100

    splitting name and ident

--- config_macro/src/config_enum_macro_derive.rs
+++ config_macro/src/config_enum_macro_derive.rs
@@ -4,7 +4,8 @@ use quote::quote;
 
 pub fn impl_config_macro(ast: &syn::DeriveInput) -> TokenStream {
     
-    let name = &ast.ident;
+    let ident = &ast.ident;
+    let name = ident.to_string();
     let syn::Data::Enum(data_enum) = &ast.data else { panic!("alarm, not an enum"); };
     let variant_parser: Vec<_> = data_enum.variants.iter().map(|v| if let Fields::Unit = v.fields {
         let ident = &v.ident;
@@ -15,11 +16,11 @@ pub fn impl_config_macro(ast: &syn::DeriveInput) -> TokenStream {
     } else { panic!("fields must be units") }).collect();
 
     quote! {
-        impl ConfigEnum for #name {
+        impl ConfigEnum for #ident {
             fn parse_config<'a>(mut values: impl Iterator<Item = &'a str>) -> Result<Self, String> {
                 match values.next().ok_or(format!("Missing enum value for {}", #name))? {
                     #( #variant_parser )*
-                    df => Err(format!("Invalid value: {}", df))
+                    df => Err(format!("Invalid value for enum {}: {}", #name, df))
                 }
             }
         }
